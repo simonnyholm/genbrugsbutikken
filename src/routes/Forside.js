84fd6592bd215c2b5ba09c6137444d09d013ee6d
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-05-25 10:03:28 +0200

    add backgr images

--- src/routes/Forside.js
+++ src/routes/Forside.js
@@ -1,5 +1,14 @@
 const Forside = () => {
-  return <div></div>;
+ const [products, setProducts] = useState(null)
+ 
+
+  return (
+    <div className="home">
+      {isPending && <div>Blogindlæg indlæses...</div>}
+      {blogs && <BlogList blogs={blogs} title="Alle blogindlæg" />}
+      {error && <div>{error}</div>}
+    </div>
+  );
 };
 
 export default Forside;
